c28fb1ddd27c2b2dacc6615c4cc1f2a8910d7ca3
Sohil Kshirsagar <sohil@usetusk.ai>
2025-06-17 18:03:49 -0700

    Added utils

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -3,3 +3,9 @@ def capitalize_first_letter(string):
     Capitalize the first letter of a string
     """
     return string[0].upper() + string[1:]
+
+
+def capitalize_last_letter(string):
+    return string[:-1] + string[-1].upper()
+
+
