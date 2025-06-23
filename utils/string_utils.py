7dc445b4e4234c3e083f18826c9a0f55ffba6e4e
Sohil Kshirsagar <sohil@usetusk.ai>
2025-06-23 13:01:38 -0700

    more utils

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -7,3 +7,8 @@ def capitalize_first_letter(string):
 def capitalize_last_letter(string):
     return string[:-1] + string[-1].upper()
 
+def capitalize_first_and_last_letter(string):
+    """
+    Capitalize the first letter of the first word and the last word of a string
+    """
+    return string[0].upper() + string[1:-1] + string[-1].upper()
