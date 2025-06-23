f4bf8bcd176f4a2a32f34700f5b167f183409a65
Sohil Kshirsagar <sohil@usetusk.ai>
2025-06-23 12:23:18 -0700

    added another util

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -7,3 +7,5 @@ def capitalize_first_letter(string):
 def capitalize_last_letter(string):
     return string[:-1] + string[-1].upper()
 
+def capitalize_first_and_last_letter(string):
+    return string[0].upper() + string[1:-1] + string[-1].upper()
